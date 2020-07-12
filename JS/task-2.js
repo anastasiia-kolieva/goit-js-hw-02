4c325b3fdd6ccf1e2bb24fe91ee3316dd973d8b3
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 01:46:05 +0300

    found trables with sorting out elements at masive

--- JS/task-2.js
+++ JS/task-2.js
@@ -5,44 +5,44 @@ let amountOfWord;
 let total;
 
 const calculateEngravingPrice = function (message, pricePerWord) {
-
-    // разбила масив по пробелам, создался новый масив wordOfEngraving
+  // разбила масив по пробелам, создался новый масив wordOfEngraving
   wordOfEngraving = message.split(" ");
 
-//   перебираю масив чтоб узнать количество слов
+  //   перебираю масив чтоб узнать количество слов???????
   for (let i = 1; i < wordOfEngraving.length; i += 1) {
-    amountOfWord = i;
+    amountOfWord += i;
+    console.log(amountOfWord);
+    break;
   }
 
-// сумма заказа=количество слов*цена за гравировку одного слова(pricePerWord)
+  // сумма заказа=количество слов*цена за гравировку одного слова(pricePerWord)
   total = amountOfWord * pricePerWord;
+  console.log(total);
 
   return total;
 };
 
-console.log(total);
-
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-// console.log(
-//   calculateEngravingPrice(
-//     "Proin sociis natoque et magnis parturient montes mus",
-//     10
-//   )
-// ); // 80
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
 
-// console.log(
-//   calculateEngravingPrice(
-//     "Proin sociis natoque et magnis parturient montes mus",
-//     20
-//   )
-// ); // 160
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
 
-// console.log(
-//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
-// ); // 240
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
 
-// console.log(
-//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
-// ); // 120
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
