e6439ca9fffa22f92597d6b093fe3a5c8eae0011
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-12 21:26:12 +0300

    have problems with task-1

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let array = [];
+let message;
+let valueOfElement;
+
+const logItems = function (array) {
+  for (const element of array) {
+    valueOfElement = element;
+  }
+
+  for (let i = 1; i < array.length; i += 1) {
+    message = `${i} - ${valueOfElement}`;
+  }
+
+  return;
+};
+
+console.log(message);
+
+logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
