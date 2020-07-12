66e8e9c9cf59b5bddc6e8188e41aa08de04f5a79
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 01:29:58 +0300

    made corrections at task-1. Have problem with list of elements at console

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,26 +1,28 @@
 "use strict";
 
-let array = [];  
+let array = [];
 let message;
+let numberOfElement;
 let valueOfElement;
 
 const logItems = function (array) {
-
   // с масива пытаюсь вытянуть елементы и внести в переменную valueOfElement
   for (const element of array) {
     valueOfElement = element;
+    continue;
   }
 
-  // перебираю масив и получаю необходимое сообщение
-  for (let i = 1; i < array.length; i += 1) {
-    message = `${i} - ${valueOfElement}`;
+  // перебираю масив чтоб получить порядковый номер
+  for (let i = 0; i < array.length; i += 1) {
+    numberOfElement = i + 1;
+    continue;
   }
 
-  return message;
+  message = `${numberOfElement} - ${valueOfElement}`;
+  console.log(message);
+
+  return;
 };
-
-console.log(message);
-// в девтулзах показывает underfined и проблема в строке 22.
 
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
