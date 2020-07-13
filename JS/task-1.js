c06f79543c2424fd450076aaf9988a2fb18d874f
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 12:52:29 +0300

    made changing at construction FOR

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,28 +1,20 @@
 "use strict";
-
 let array = [];
 let message;
-let numberOfElement;
-let valueOfElement;
 
 const logItems = function (array) {
-  // с масива пытаюсь вытянуть елементы и внести в переменную valueOfElement
-  for (const element of array) {
-    valueOfElement = element;
-    continue;
-  }
-
-  // перебираю масив чтоб получить порядковый номер
   for (let i = 0; i < array.length; i += 1) {
-    numberOfElement = i + 1;
-    continue;
-  }
 
-  message = `${numberOfElement} - ${valueOfElement}`;
-  console.log(message);
+    message = `${i + 1} - ${array[0]}`;
+    console.log(message);
+  }
 
   return;
 };
 
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
