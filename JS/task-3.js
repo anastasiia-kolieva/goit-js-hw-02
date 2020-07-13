fc13bc9ec9f8007f1aa56ac92c9f2a6435475ad5
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 14:13:16 +0300

    tried to made task-3

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,26 +1,31 @@
 "use strict";
 
 let listOfWords;
-let index;
+let lengthOfWord;
 
 const findLongestWord = function (string) {
   // разбила строку по пробелам на слова. в результате получила масив listOfWords
   listOfWords = string.split(" ");
 
-  for (const oneWord of listOfWords) {
-    // если индекс последнего символа слова равен длинне слова тогда вывести это слово?????
-    if (oneWord.lastIndexOf() - 1 === oneWord.length) {
-      console.log(oneWord);
+  for (const word of listOfWords) {
+    // console.log(word);
+
+    for (let i = 0; i < word.length; i += 1) {
+      lengthOfWord = word.length[i];
+
+      if(lengthOfWord)
     }
   }
+
   return;
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
+
 findLongestWord("The quick brown fox jumped over the lazy dog"); // 'jumped'
 
-findLongestWord("Google do a roll"); // 'Google'
+// findLongestWord("Google do a roll"); // 'Google'
 
-findLongestWord("May the force be with you"); // 'force'
+// findLongestWord("May the force be with you"); // 'force'
