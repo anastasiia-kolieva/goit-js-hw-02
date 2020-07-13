a5ba6293aadcd66103e032a3cbddf25b81e38baf
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 21:16:34 +0300

    started to do task-6

--- JS/task-6.js
+++ JS/task-6.js
@@ -1,1 +1,53 @@
 "use strict";
+
+let input;
+const numbers = [];
+let total = 0;
+
+input = prompt("Введите число");
+numbers.push(input);
+
+const addNumber = function(numbers){
+
+    for (let i=0; i<numbers.length; i+=1){
+
+    if (input === null) {
+        console.log("Отменено пользователем!");
+        break;
+      }
+    
+      if (Number.isNaN(Number(input))) {
+        alert("Было введено не число, попробуйте еще раз");
+        continue;
+      } else {
+        total += Number(input);
+        console.log(total);
+        break;
+      }
+    }
+
+    return;
+}
+
+console.log (`Общая сумма чисел равна ${total}`);
+
+
+
+
+
+// do {
+
+//   if (input === null) {
+//     console.log("Отменено пользователем!");
+//     break;
+//   }
+
+//   if (Number.isNaN(Number(input))) {
+//     alert("Было введено не число, попробуйте еще раз");
+//     continue;
+//   } else {
+//     total += Number(input);
+//     console.log(total);
+//     break;
+//   }
+// } while (true);
