f4e233d1a0e6958d5ee20fc901680d672d462c95
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-13 16:33:50 +0300

    made first part of task-4. Have problem with split string

--- JS/task-4.js
+++ JS/task-4.js
@@ -1,1 +1,41 @@
 "use strict";
+
+let lengthOfString;
+let arr = [];
+let partOfString;
+let message;
+
+const formatString = function (string) {
+  // Определила длинну строк, вывела в консоль
+  lengthOfString = string.length;
+  console.log(lengthOfString);
+
+  //   Если длинна строки равна и меньше 40, возвращаем в консоль строку в начальном виде
+  if (lengthOfString <= 40) {
+    console.log(string);
+  }
+  if (lengthOfString > 40) {
+
+    // ???? не знаю как ограничить вывод только 40-ка символов в строке
+  }
+  return;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// вернется оригинальная строка
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// вернется форматированная строка
+
+// console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
+
+// console.log(
+//   formatString(
+//     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+//   ),
+// );
+// вернется форматированная строка
