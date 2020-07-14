8e2eab3d2ef7d158b09ca800d6aeef15e23f5408
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-14 23:50:21 +0300

    made task-3

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,33 +1,21 @@
 "use strict";
 
 let list;
-let lengthOfWord;
 let longestWord;
 
 const findLongestWord = function (string) {
-  // разбила строку по пробелам на слова. в результате получила масив listOfWords
   list = string.split(" ");
-
-  for (const word of list) {
-    lengthOfWord = word.length;
-  }
   longestWord = list[0];
 
-  for (let i = 0; i < list.length; i += 1) {
-    // в начале перебора цикла самое длинное слово, это первое слово
-
-    // если длина елемента масива меньше чем длина СЛЕДУЮЩЕНО елемента этого же масива
-    if (lengthOfWord < lengthOfWord[i]) {
+  for (let i = 1; i < list.length; i += 1) {
+    if (list[i].length > longestWord.length) {
       longestWord = list[i];
-      break;
     }
-
-    // в console выводится 9 штук первого слова. я так понимаю что условие IF не срабатывает.
-    console.log(longestWord);
   }
-
-  return;
+  console.log(longestWord);
+  return longestWord;
 };
+
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
@@ -35,6 +23,6 @@ const findLongestWord = function (string) {
 
 findLongestWord("The quick brown fox jumped over the lazy dog"); // 'jumped'
 
-// findLongestWord("Google do a roll"); // 'Google'
+findLongestWord("Google do a roll"); // 'Google'
 
-// findLongestWord("May the force be with you"); // 'force'
+findLongestWord("May the force be with you"); // 'force'
